fe0e77dc42b1ac53a0905f35af059fdf6d32814a
YoelvzZ <35924066+YoelvzZ@users.noreply.github.com>
2026-02-26 22:55:53 -0500

    Seguridad: Credenciales movidas a .env y protegidas por gitignore

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// PEGA AQUÍ TU CONFIGURACIÓN DE FIREBASE
+// Configuración protegida mediante variables de entorno de Expo
 const firebaseConfig = {
-  apiKey: "AIzaSyBboftGuy3GM5AGTCsds7vrfhv-X4C5toI",
-  authDomain: "laundrymanager-a6fa2.firebaseapp.com",
-  projectId: "laundrymanager-a6fa2",
-  storageBucket: "laundrymanager-a6fa2.firebasestorage.app",
-  messagingSenderId: "94545827994",
-  appId: "1:94545827994:web:a7e011ed0288252548a723"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Inicializar Firebase
